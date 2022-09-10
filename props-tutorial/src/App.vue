d2470860f4fe268064546c2a82262acf5e836d84
Sedanur <50722053+Sedanurr@users.noreply.github.com>
2022-09-11 00:53:16 +0300

    props

--- props-tutorial/src/App.vue
+++ props-tutorial/src/App.vue
@@ -1,20 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <HeaderComponent />
+    <NinjasComponent :ninjas="ninjas"/>
+    <FooterComponent />
   </div>
 </template>
 
 <script>
-
+import FooterComponent from "./components/FooterComponent.vue";
+import HeaderComponent from "./components/HeaderComponent.vue";
+import NinjasComponent from "./components/NinjasComponent.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-
-  }
-}
+    FooterComponent,
+    HeaderComponent,
+    NinjasComponent,
+  },
+  data() {
+    return {
+      ninjas: [
+        { name: "Ryu", speciality: "Vue Components", show: false },
+        { name: "Crystal", speciality: "HTML Wizardry", show: false },
+        { name: "Hitoshi", speciality: "Click Events", show: false },
+        { name: "Tango", speciality: "Conditionals", show: false },
+        { name: "Kami", speciality: "Webpack", show: false },
+        { name: "Yoshi", speciality: "Data Diggin", show: false },
+      ],
+    };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
