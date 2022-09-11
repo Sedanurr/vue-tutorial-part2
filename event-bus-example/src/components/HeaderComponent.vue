2e3e1b1083659add4edbcd8dd3a01ff5e6536a74
Sedanur <50722053+Sedanurr@users.noreply.github.com>
2022-09-11 22:25:39 +0300

    tutorial-25

---
+++ event-bus-example/src/components/HeaderComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <header>
+    <h1 v-on:click="changeTitle">{{ title }}</h1>
+  </header>
+</template>
+<script>
+import { bus } from "../main";
+export default {
+  props: {
+    title: {
+      type: String,
+    },
+  },
+  data() {
+    return {
+      titlePri: "vue ninja",
+    };
+  },
+  methods: {
+    changeTitle: function () {
+      // this.$emit("changeTitle", "Vue Wizards");
+      this.title = 'Vue Wizards' 
+      bus.$emit('titleChanged','Vue Wizards')
+    },
+  },
+};
+</script>
+<style scoped>
+header {
+  background: lightgreen;
+  padding: 10px;
+}
+h1 {
+  text-align: center;
+  color: #222;
+}
+</style>
