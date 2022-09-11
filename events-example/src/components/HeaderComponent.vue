18b77305a1f7d559122ae352715f9ba4e14760a0
Sedanur <50722053+Sedanurr@users.noreply.github.com>
2022-09-11 22:10:56 +0300

    tutorial-24

---
+++ events-example/src/components/HeaderComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    <h1 v-on:click="changeTitle">{{ title }}</h1>
+  </header>
+</template>
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+    },
+  },
+  data() {
+    return {
+      titlePri: "vue ninja",
+    };
+  },
+  methods: {
+    changeTitle: function () {
+      this.$emit('changeTitle','Vue Wizards')
+    },
+  },
+};
+</script>
+<style scoped>
+header {
+  background: lightgreen;
+  padding: 10px;
+}
+h1 {
+  text-align: center;
+  color: #222;
+}
+</style>
