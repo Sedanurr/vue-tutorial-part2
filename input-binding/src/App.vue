155e022ff7618cc47eca1a7398a199abc6522f90
Sedanur <50722053+Sedanurr@users.noreply.github.com>
2022-09-13 10:18:25 +0300

    tutorial-29

---
+++ input-binding/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <AddBlog />
+  </div>
+</template>
+
+<script>
+import AddBlog from "./components/addBlog.vue";
+
+export default {
+  name: "App",
+  components: {
+    AddBlog,
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: "Nunito SemiBold";
+}
+</style>
