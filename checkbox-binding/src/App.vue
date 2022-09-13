8e2121b791ddbcf704c7b2347603659e7f05f630
Sedanur <50722053+Sedanurr@users.noreply.github.com>
2022-09-13 10:32:46 +0300

    tutorial-30

---
+++ checkbox-binding/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <AddBlog />
+  </div>
+</template>
+
+<script>
+import AddBlog from "./components/addBlog.vue";
+
+export default {
+  name: "App",
+  components: {
+    AddBlog,
+  },
+};
+</script>
+
+<style></style>
