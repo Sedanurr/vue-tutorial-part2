8688fe39ab40ecb630a7687d77ad01a6dcce801c
Sedanur <50722053+Sedanurr@users.noreply.github.com>
2022-09-14 22:07:02 +0300

    tutorial-32

---
+++ http-requests/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+  <AddBlog></AddBlog>
+  </div>
+</template>
+
+<script>
+import AddBlog from './components/addBlog.vue';
+export default {
+  name: "App",
+  components: { AddBlog },
+};
+</script>
+
+<style></style>
