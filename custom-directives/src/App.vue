756b308b01e15b836b1413674ce21a104bc7b0ab
Sedanur <50722053+Sedanurr@users.noreply.github.com>
2022-09-14 22:52:50 +0300

    tutorial-34

---
+++ custom-directives/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <ShowBlogs />
+  </div>
+</template>
+
+<script>
+import ShowBlogs from "../src/components/showBlogs.vue";
+
+export default {
+  name: "App",
+  components: { ShowBlogs },
+};
+</script>
+
+<style></style>
---
+++ custom-directives/src/components/showBlogs.vue
@@ -0,0 +1,38 @@
+<template>
+  <div v-theme:column="'wide'" id="show-blogs">
+    <h1>All Blog Articles</h1>
+    <div v-for="(blog, index) in blogs" :key="index" class="single-blog">
+      <h2 v-rainbow>{{ blog.title }}</h2>
+      <article>{{ blog.body }}</article>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      blogs: [],
+    };
+  },
+  methods: {},
+  created() {
+    this.$http
+      .get("http://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        this.blogs = response.body.slice(0, 10);
+      });
+  },
+};
+</script>
+<style>
+#show-blogs {
+  max-width: 800px;
+  margin: 0 auto;
+}
+.single-blog {
+  padding: 20px;
+  box-sizing: border-box;
+  margin: 20px 0;
+  background: #eee;
+}
+</style>
