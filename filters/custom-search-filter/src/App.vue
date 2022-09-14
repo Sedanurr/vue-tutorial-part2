6af43f1ee85cc17f2418ec54fff65c139fd85a8d
Sedanur <50722053+Sedanurr@users.noreply.github.com>
2022-09-14 23:20:06 +0300

    tutorial-36

---
+++ filters/custom-search-filter/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <ShowBlogs></ShowBlogs>
+  </div>
+</template>
+
+<script>
+import ShowBlogs from "./components/showBlogs.vue";
+
+export default {
+  name: "App",
+  components: {
+    ShowBlogs,
+  },
+};
+</script>
+
+<style></style>
