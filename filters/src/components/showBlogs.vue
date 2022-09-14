1aeb9881ad0caae9a12e7d23f83c8cc5c06df2f4
Sedanur <50722053+Sedanurr@users.noreply.github.com>
2022-09-14 23:05:47 +0300

    tutorial-35

---
+++ filters/src/components/showBlogs.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="show-blogs">
+    <h1>All Blog Articles</h1>
+    <div v-for="(blog, index) in blogs" :key="index" class="single-blog">
+      <h2>{{ blog.title | to-uppercase }}</h2>
+      <article>{{ blog.body | snippet }}</article>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      blogs: [],
+    };
+  },
+  methods: {},
+  created() {
+    this.$http
+      .get("http://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        this.blogs = response.body.slice(0, 10);
+      });
+  },
+};
+</script>
+<style>
+#show-blogs {
+  max-width: 800px;
+  margin: 0 auto;
+}
+.single-blog {
+  padding: 20px;
+  box-sizing: border-box;
+  margin: 20px 0;
+  background: #eee;
+}
+</style>
---
+++ filters/src/main.js
@@ -0,0 +1,18 @@
+import Vue from "vue";
+import App from "./App.vue";
+import VueResource from "vue-resource";
+
+Vue.use(VueResource);
+
+Vue.filter('to-uppercase',function(value) {
+  return value.toUpperCase()
+})
+Vue.filter('snippet',function(value) {
+  return value.slice(0,100) + '...'
+})
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: (h) => h(App),
+}).$mount("#app");
