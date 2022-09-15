38dbafe50f186881afc15503856e735105ca4681
Sedanur <50722053+Sedanurr@users.noreply.github.com>
2022-09-16 01:25:45 +0300

    tutorial-41

---
+++ adding-router-links/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+      <HeaderComponent />
+      <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+import HeaderComponent from './components/headerComponent.vue';
+
+export default {
+  components: {
+    HeaderComponent
+},
+  data () {
+      return {
+
+      }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+body{
+  margin: 0;
+  font-family: 'Nunito SemiBold';
+}
+</style>
