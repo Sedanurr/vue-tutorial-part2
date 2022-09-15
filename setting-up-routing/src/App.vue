485222275f9d136b09a401a9d4dd340a89cc01ab
Sedanur <50722053+Sedanurr@users.noreply.github.com>
2022-09-16 01:03:27 +0300

    tutorial-39

---
+++ setting-up-routing/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: "Nunito SemiBold";
+}
+</style>
