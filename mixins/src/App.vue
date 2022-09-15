f7518fe11acaa4651fc27e0b107231cefd520646
Sedanur <50722053+Sedanurr@users.noreply.github.com>
2022-09-16 00:24:59 +0300

    tutorial-38

---
+++ mixins/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <ShowBlogs />
+    <ListBlogs />
+  </div>
+</template>
+
+<script>
+// Imports
+import ShowBlogs from "./components/showBlogs.vue";
+import ListBlogs from "./components/listBlogs.vue";
+
+export default {
+  components: {
+    ShowBlogs,
+    ListBlogs,
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: "Nunito SemiBold";
+}
+</style>
