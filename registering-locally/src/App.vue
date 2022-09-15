48f09458e2970b8e2cf271550382b8f63a46847a
Sedanur <50722053+Sedanurr@users.noreply.github.com>
2022-09-16 00:12:34 +0300

    tutorial-37

---
+++ registering-locally/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <ShowBlogs />
+  </div>
+</template>
+
+<script>
+import ShowBlogs from "./components/showBlogs.vue";
+
+export default {
+  name: "App",
+  components: {
+    ShowBlogs,
+  },
+};
+</script>
+
+<style>
+</style>
