5d03533ed8803fa056c7ae9abf2b4103957fc9a4
Sedanur <50722053+Sedanurr@users.noreply.github.com>
2022-09-19 10:55:25 +0300

    tutorial-42

---
+++ route-parameters/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <AppHeader />
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import AppHeader from "./components/appHeader.vue";
+
+export default {
+  components: {
+    AppHeader,
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: "Nunito SemiBold";
+}
+</style>
