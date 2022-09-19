9aedea9bfd55c3210ea7bab110aa08e81adfd085
Sedanur <50722053+Sedanurr@users.noreply.github.com>
2022-09-19 12:17:08 +0300

    tutorial-44

---
+++ posts-from-firebase/src/components/singleBlog.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="single-blog">
+    <h1>{{ blog.title }}</h1>
+    <article>{{ blog.content }}</article>
+    <p>Author: {{ blog.author }}</p>
+    <p>Categories:</p>
+    <ul>
+      <li v-for="(category,index) in blog.categories" :key="index">{{ category }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      id: this.$route.params.id,
+      blog: {},
+    };
+  },
+  created() {
+    this.$http
+      .get("https://vue2-tutorial-33701-default-rtdb.firebaseio.com//posts/" + this.id + ".json")
+      .then(function (data) {
+        return data.json();
+      })
+      .then(function (data) {
+        this.blog = data;
+      });
+  },
+};
+</script>
+
+<style>
+#single-blog {
+  max-width: 960px;
+  margin: 0 auto;
+  padding: 20px;
+  background: #eee;
+  border: 1px dotted #aaa;
+}
+</style>
